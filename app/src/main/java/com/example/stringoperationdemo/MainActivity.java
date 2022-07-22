22e5db4c797f3a4cd9536ac17eb2eadcff99954a
Lorand-F <formagiu.lorand@gmail.com>
2022-07-22 20:52:23 +0300

    Phase 2. Java XML code binding.

--- app/src/main/java/com/example/stringoperationdemo/MainActivity.java
+++ app/src/main/java/com/example/stringoperationdemo/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.stringoperationdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btRemoveString, btRemoveCharacter, btSplitString, btGetUrlString;
+    TextView tvOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btRemoveString=findViewById(R.id.bt_remove_string);
+        btRemoveCharacter=findViewById(R.id.bt_remove_character);
+        btSplitString=findViewById(R.id.bt_split_string);
+        btGetUrlString=findViewById(R.id.bt_get_url_string);
+        tvOutput=findViewById(R.id.tv_output);
     }
 }
