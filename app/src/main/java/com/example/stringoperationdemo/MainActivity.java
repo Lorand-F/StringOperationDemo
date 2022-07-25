b708b6ebbc856a0d5af04ba6ecf6da33f9651f72
Lorand-F <formagiu.lorand@gmail.com>
2022-07-25 20:00:12 +0300

    Phase 4. btSplitString and add a new TextView with text before by change by button.

--- app/src/main/java/com/example/stringoperationdemo/MainActivity.java
+++ app/src/main/java/com/example/stringoperationdemo/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Button btRemoveString, btRemoveCharacter, btSplitString, btGetUrlString;
-    TextView tvOutput;
+    TextView tvOutputBefore, tvOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +21,40 @@ public class MainActivity extends AppCompatActivity {
         btRemoveCharacter=findViewById(R.id.bt_remove_character);
         btSplitString=findViewById(R.id.bt_split_string);
         btGetUrlString=findViewById(R.id.bt_get_url_string);
+        tvOutputBefore=findViewById(R.id.tv_output_before);
         tvOutput=findViewById(R.id.tv_output);
 
         btRemoveString.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String s="Channel Name : Android Application";
+                tvOutputBefore.setText(s);
                 tvOutput.setText(s);
                 String removedString=s.replaceAll("Channel Name : ","");
                 tvOutput.setText(removedString);
+            }
+        });
+
+        btRemoveCharacter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s="(19.1234,73.4321)";
+                tvOutputBefore.setText(s);
+                String removedLeftCharacter=s.replace("(","");
+                String removedRightCharacter=removedLeftCharacter.replace(")","");
+                tvOutput.setText(removedRightCharacter);
+            }
+        });
+
+        btSplitString.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s="19.4321,73.1234";
+                tvOutputBefore.setText(s);
+                String[] split=s.split(",");
+                String s1=split[0];
+                String  s2=split[1];;
+                tvOutput.setText(String.format("%s\n%s", s1, s2));
             }
         });
     }
